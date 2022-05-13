c8760bc0c843aa1e41ea93309b8e5b6ce42d080c
904188001 <904188001@L815WD220017.edu.cbe.ab.ca>
2022-05-13 09:45:18 -0600

    thumbstick

--- Phidgets/src/phidgetScale/Thumbstick.java
+++ Phidgets/src/phidgetScale/Thumbstick.java
@@ -8,13 +8,58 @@ import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
 
+import com.phidget22.VoltageRatioInput;
 
 import java.awt.Color;
 
 public class Thumbstick {
 
 	private JFrame frame;
-	JLabel circle = new JLabel("");
+	public JLabel circle = new JLabel("");
+	
+
+	/**
+	 * Create the application.
+	 */
+	public Thumbstick(){
+		initialize();
+	}
+
+	
+	public void moveThumbstick() throws Exception
+	{
+		circle.setBounds(150, 150, 100, 100);
+		
+		 VoltageRatioInput vAxis = new VoltageRatioInput(); 
+	     VoltageRatioInput hAxis = new VoltageRatioInput();
+	     
+	     vAxis.setChannel(0);
+	     hAxis.setChannel(1);
+	     
+	     vAxis.open(5000);
+	     hAxis.open(5000);
+	     
+	     while(true)
+	     {
+	    	 double verticalAxis = vAxis.getVoltageRatio();
+	          
+	         double horizontalAxis = hAxis.getVoltageRatio();
+	        
+	         int yPosition = (int) verticalAxis * 400;
+	         
+	         int xPosition = (int) horizontalAxis * 400;
+	         
+	         if(Math.abs(verticalAxis) > 0.01 || Math.abs(horizontalAxis) > 0.01)
+	         {
+	        	 circle.setBounds(xPosition, yPosition, 100, 100);
+	         }
+	         else
+	         {
+	        	 circle.setBounds(150, 150, 100, 100);
+	         }
+	     }
+	}
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -35,22 +80,7 @@ public class Thumbstick {
 		circle.setIcon(new ImageIcon("C:\\Users\\904188001\\git\\CS30\\Phidgets\\circle.jpg"));
 		circle.setHorizontalAlignment(SwingConstants.CENTER);
 		panel.add(circle);
-		
 	}
-
-	public void moveThumstick() throws Exception
-	{
-	
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public Thumbstick() {
-		initialize();
-	}
-
-	
 	
 	/**
 	 * Launch the application.
